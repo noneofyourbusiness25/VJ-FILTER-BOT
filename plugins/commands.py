2dbdc585c6f716830e62970e384e538214d75171
noneofyourbusiness25 <71598767+noneofyourbusiness25@users.noreply.github.com>
2024-09-15 19:17:06 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -162,20 +162,15 @@ async def start(client, message):
             parse_mode=enums.ParseMode.HTML
         )
         return
+    
     data = message.command[1]
-    user_id = int(data.split("-", 1)[1])
-
-    # Check for self-referral
-    if message.from_user.id == user_id:
-        await message.reply("<b>You cannot refer yourself!</b>")
-        return
-
     if data.split("-", 1)[0] == "VJ":
-        vj = await referal_add_user(message, user_id, message.from_user.id)
+        user_id = int(data.split("-", 1)[1])
+        vj = await referal_add_user(user_id, message.from_user.id)
         if vj and PREMIUM_AND_REFERAL_MODE == True:
             await message.reply(f"<b>You have joined using the referral link of user with ID {user_id}\n\nSend /start again to use the bot</b>")
             num_referrals = await get_referal_users_count(user_id)
-            await client.send_message(chat_id=user_id, text="<b>{} started the bot with your referral link\n\nTotal Referrals - {}</b>".format(message.from_user.mention, num_referrals))
+            await client.send_message(chat_id = user_id, text = "<b>{} start the bot with your referral link\n\nTotal Referals - {}</b>".format(message.from_user.mention, num_referrals))
             if num_referrals == REFERAL_COUNT:
                 time = REFERAL_PREMEIUM_TIME       
                 seconds = await get_seconds(time)
@@ -185,7 +180,8 @@ async def start(client, message):
                     await db.update_user(user_data)  # Use the update_user method to update or insert user data
                     await delete_all_referal_users(user_id)
                     await client.send_message(chat_id = user_id, text = "<b>You Have Successfully Completed Total Referal.\n\nYou Added In Premium For {}</b>".format(REFERAL_PREMEIUM_TIME))
-                    return            
+                    return       
+                    
         else:
             if PREMIUM_AND_REFERAL_MODE == True:
                 buttons = [[
