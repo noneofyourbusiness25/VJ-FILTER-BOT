5e0d2479d88edf1a101543ccbd7426615d78f84a
noneofyourbusiness25 <71598767+noneofyourbusiness25@users.noreply.github.com>
2024-09-16 10:47:14 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -164,10 +164,10 @@ async def start(client, message):
         return
     data = message.command[1]
     if data.split("-", 1)[0] == "VJ":
-    if user_id == message.from_user.id:
-        await message.reply("<b>You cannot refer yourself!</b>")
-        return
         user_id = int(data.split("-", 1)[1])
+        if user_id == message.from_user.id:
+            await message.reply("<b>You cannot refer yourself!</b>")
+            return
         vj = await referal_add_user(user_id, message.from_user.id)
         if vj and PREMIUM_AND_REFERAL_MODE == True:
             await message.reply(f"<b>You have joined using the referral link of user with ID {user_id}\n\nSend /start again to use the bot</b>")
