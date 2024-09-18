24280926a601ceab24ecee1880f827f8699850c0
noneofyourbusiness25 <71598767+noneofyourbusiness25@users.noreply.github.com>
2024-09-18 09:50:50 +0530

    Update approve.py

--- plugins/Extra/auto_approve/approve.py
+++ plugins/Extra/auto_approve/approve.py
@@ -28,38 +28,64 @@ async def auto_approve(client, message: ChatJoinRequest):
         await join_db().add_user(user_id=ap_user_id, first_name=first_name, username=username, date=date)
         if TRY_AGAIN_BTN == True:
             return 
-        if data.split('-', 1)[0] == 'VJ':
-            user_id = int(data.split('-', 1)[1])
-            
-            # Ensure both user_id and message.from_user.id are integers for comparison
-            if int(user_id) == int(message.from_user.id):
-                await client.send_message(message.from_user.id, '<b>You cannot refer yourself!</b>')
-                return
-
-            # Continue with referral processing only if the self-referral check passes
-            vj = await referal_add_user(message, user_id, message.from_user.id)
+        data = await db.get_msg_command(ap_user_id)
+        
+        if data.split("-", 1)[0] == "VJ":
+            user_id = int(data.split("-", 1)[1])
+            vj = await referal_add_user(user_id, message.from_user.id)
             if vj and PREMIUM_AND_REFERAL_MODE == True:
-                await client.send_message(message.from_user.id, f'<b>You have joined using the invite link of user with ID {user_id}\n\nSend /start again to use the bot</b>')
+                await client.send_message(message.from_user.id, f"<b>You have joined using the referral link of user with ID {user_id}\n\nSend /start again to use the bot</b>")
                 num_referrals = await get_referal_users_count(user_id)
-                await client.send_message(chat_id=user_id, text=f'<b>{message.from_user.mention} started the bot with your referral link\n\nTotal Referrals - {num_referrals}</b>')
+                await client.send_message(chat_id = user_id, text = "<b>{} start the bot with your referral link\n\nTotal Referals - {}</b>".format(message.from_user.mention, num_referrals))
                 if num_referrals == REFERAL_COUNT:
-                    time = REFERAL_PREMIUM_TIME
+                    time = REFERAL_PREMEIUM_TIME       
                     seconds = await get_seconds(time)
                     if seconds > 0:
                         expiry_time = datetime.datetime.now() + datetime.timedelta(seconds=seconds)
-                        user_data = {'id': user_id, 'expiry_time': expiry_time}
-                        await db.update_user(user_data)
+                        user_data = {"id": user_id, "expiry_time": expiry_time} 
+                        await db.update_user(user_data)  # Use the update_user method to update or insert user data
                         await delete_all_referal_users(user_id)
-                        await client.send_message(chat_id=user_id, text=f'<b>You have successfully completed the total referrals.\n\nYou have been added to premium for {REFERAL_PREMIUM_TIME}</b>')
-            await client.send_message(chat_id=user_id, text=f"<b>You have successfully completed the total referrals.\n\nYou have been added to premium for {REFERAL_PREMIUM_TIME}</b>")
-            await client.send_photo(
-                chat_id=message.from_user.id,
-                photo=random.choice(PICS),
-                caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
-                reply_markup=reply_markup,
-                parse_mode=enums.ParseMode.HTML
-            )
-            return
+                        await client.send_message(chat_id = user_id, text = "<b>You Have Successfully Completed Total Referal.\n\nYou Added In Premium For {}</b>".format(REFERAL_PREMEIUM_TIME))
+                        return 
+            else:
+                if PREMIUM_AND_REFERAL_MODE == True:
+                    buttons = [[
+                        InlineKeyboardButton('⤬ Aᴅᴅ Mᴇ Tᴏ Yᴏᴜʀ Gʀᴏᴜᴘ ⤬', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
+                    ],[
+                        InlineKeyboardButton('Eᴀʀɴ Mᴏɴᴇʏ 💸', callback_data="shortlink_info"),
+                        InlineKeyboardButton('⌬ Mᴏᴠɪᴇ Gʀᴏᴜᴘ', url=GRP_LNK)
+                    ],[
+                        InlineKeyboardButton('〄 Hᴇʟᴘ', callback_data='help'),
+                        InlineKeyboardButton('⍟ Aʙᴏᴜᴛ', callback_data='about')
+                    ],[
+                        InlineKeyboardButton('🔻 ɢᴇᴛ ғʀᴇᴇ/ᴘᴀɪᴅ sᴜʙsᴄʀɪᴘᴛɪᴏɴ 🔻', callback_data='subscription')
+                    ],[
+                        InlineKeyboardButton('✇ Jᴏɪɴ Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ ✇', url=CHNL_LNK)
+                    ]]
+                else:
+                    buttons = [[
+                        InlineKeyboardButton('⤬ Aᴅᴅ Mᴇ Tᴏ Yᴏᴜʀ Gʀᴏᴜᴘ ⤬', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
+                    ],[
+                        InlineKeyboardButton('Eᴀʀɴ Mᴏɴᴇʏ 💸', callback_data="shortlink_info"),
+                        InlineKeyboardButton('⌬ Mᴏᴠɪᴇ Gʀᴏᴜᴘ', url=GRP_LNK)
+                    ],[
+                        InlineKeyboardButton('〄 Hᴇʟᴘ', callback_data='help'),
+                        InlineKeyboardButton('⍟ Aʙᴏᴜᴛ', callback_data='about')
+                    ],[
+                        InlineKeyboardButton('✇ Jᴏɪɴ Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ ✇', url=CHNL_LNK)
+                    ]]
+                reply_markup = InlineKeyboardMarkup(buttons)
+                m=await client.send_sticker(chat_id = message.from_user.id, sticker = "CAACAgUAAxkBAAEKVaxlCWGs1Ri6ti45xliLiUeweCnu4AACBAADwSQxMYnlHW4Ls8gQMAQ") 
+                await asyncio.sleep(1)
+                await m.delete()
+                await client.send_photo(
+                    chat_id=message.from_user.id,
+                    photo=random.choice(PICS),
+                    caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
+                    reply_markup=reply_markup,
+                    parse_mode=enums.ParseMode.HTML
+                )
+                return 
         try:
             pre, file_id = data.split('_', 1)
         except:
@@ -102,7 +128,7 @@ async def auto_approve(client, message: ChatJoinRequest):
             await asyncio.sleep(300)
             await k.edit("<b>Your message is successfully deleted!!!</b>")
             return   
-
+    
         elif data.startswith("short"):
             user = message.from_user.id
             chat_id = temp.SHORT.get(user)
